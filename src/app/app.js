e2a92cde5d88112235200a9d553d0a843c1774bb
Jason Williams <jwilliams@pnca.edu>
2013-08-29 16:24:06 -0700

    redux

--- src/app/app.js
+++ src/app/app.js
@@ -1,41 +1,29 @@
 angular.module( 'sonOfCalamari', [
   'app-templates',
   'component-templates',
-  'sonOfCalamari.home',
-  'sonOfCalamari.about',
-  'sonOfCalamari.footer',
-  'ui.state' 
+  'ui.state',
+  'titleService' 
 ])
 
 .config(function($stateProvider, $urlRouterProvider){
   //
-  // For any unmatched url, send to /route1
-  $urlRouterProvider.otherwise("/route1"); 
+  // For any unmatched url, send to /list
+  $urlRouterProvider.otherwise("/list"); 
   //
   // Now set up the states
   $stateProvider
-    .state('route1', {
-      url: "/route1",
-      templateUrl: "lists/route1.tpl.html"
+    .state('detail', {
+      url: "/detail",
+      templateUrl: "events/detail.tpl.html"
     })
-    .state('route1.list', {
+    .state('list', {
       url: "/list",
-      templateUrl: "lists/route1list.tpl.html",
+      templateUrl: "events/list.tpl.html",
       controller: function($scope){
         $scope.items = ["A", "List", "Of", "Items"];
       }
-    })          
-    .state('route2', {
-       url: "/route2",
-       templateUrl: "lists/route2.tpl.html"
     })
-    .state('route2.list', {
-       url: "/list",
-       templateUrl: "lists/route2list.tpl.html",
-       controller: function($scope){
-         $scope.things = ["A", "Set", "Of", "Things"];
-       }
-     });
+  ;
 })
 
 .run( function run ( titleService ) {
